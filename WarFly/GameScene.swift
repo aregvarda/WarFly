12f54d9d682c044a8db2e6ceeac41b8d4f9991c4
Areg Vardanian <aregv@bk.ru>
2021-11-22 20:34:22 +0300

    Movement

--- WarFly/GameScene.swift
+++ WarFly/GameScene.swift
@@ -7,12 +7,16 @@
 
 import SpriteKit
 import GameplayKit
+import CoreMotion
 
 class GameScene: SKScene {
     
+    let motionManager = CMMotionManager()
+    var xAcceleration: CGFloat = 0
+    var player: SKSpriteNode!
     
     override func didMove(to view: SKView) {
-    
+        
         let screenCenterPoint = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
         let background = Background.populateBackground(at: screenCenterPoint)
         background.size = self.size
@@ -20,7 +24,7 @@ class GameScene: SKScene {
         
         let screen = UIScreen.main.bounds
         for _ in 1...5 {
-           
+            
             let x: CGFloat = CGFloat(GKRandomSource.sharedRandom().nextInt(upperBound: Int(screen.size.width)))
             let y: CGFloat = CGFloat(GKRandomSource.sharedRandom().nextInt(upperBound: Int(screen.size.height)))
             
@@ -30,5 +34,27 @@ class GameScene: SKScene {
             let cloud = Cloud.populateSprite(at: CGPoint(x: x, y: y))
             self.addChild(cloud)
         }
+        player = PlayerPlane.populate(at: CGPoint(x: screen.size.width / 2, y: 100))
+        self.addChild(player)
+        
+        motionManager.accelerometerUpdateInterval = 0.2
+        motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { (data, error) in
+            if let data = data {
+                let acceleration = data.acceleration
+                self.xAcceleration = CGFloat(acceleration.x) * 0.7 + self.xAcceleration * 0.3
+            }
+        }
+    }
+    override func didSimulatePhysics() {
+        super.didSimulatePhysics()
+        
+        player.position.x += xAcceleration * 50
+        if player.position.x < -70 {
+            player.position.x = self.size.width + 70
+        } else if player.position.x > self.size.width + 70 {
+            player.position.x = -70
+        }
     }
 }
+
+
--- WarFly/Island.swift
+++ WarFly/Island.swift
@@ -16,11 +16,12 @@ final class Island: SKSpriteNode, GameBackGroundSpriteable {
         island.position = point
         island.zPosition = 1
         island.run(rotateForRandomAngle())
+        island.run(move(from: point))
         
         return island
     }
     
-    static func configureName() -> String {
+    fileprivate static func configureName() -> String {
         let distribution = GKRandomDistribution(lowestValue: 1, highestValue: 4)
         let randomNumber = distribution.nextInt()
         let imageName = "is" + "\(randomNumber)"
@@ -28,19 +29,28 @@ final class Island: SKSpriteNode, GameBackGroundSpriteable {
         return imageName
     }
     
-    static var randomScaleFactor: CGFloat {
+    fileprivate static var randomScaleFactor: CGFloat {
         let distribution = GKRandomDistribution(lowestValue: 1, highestValue: 10)
         let randomNumber = CGFloat(distribution.nextInt()) / 10
         
         return randomNumber
     }
     
-    static func rotateForRandomAngle() -> SKAction {
+    fileprivate static func rotateForRandomAngle() -> SKAction {
         
         let distribution = GKRandomDistribution(lowestValue: 0, highestValue: 360)
         let randomNumber = CGFloat(distribution.nextInt()) / 10
         
         return SKAction.rotate(toAngle: randomNumber * CGFloat(Double.pi / 180), duration: 0)
+    }
+    
+    fileprivate static func move(from point: CGPoint) -> SKAction {
+        
+        let movePoint = CGPoint(x: point.x, y: -200)
+        let moveDistance = point.y + 200
+        let movementSpeed: CGFloat = 10.0
+        let duration = moveDistance / movementSpeed
+        return SKAction.move(to: movePoint, duration: TimeInterval(duration))
     }
 }
 
