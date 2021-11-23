d18b0a4ef1a2f3d2e1da9d43cb8bf62cd58a940c
Areg Vardanian <aregv@bk.ru>
2021-11-23 15:58:18 +0300

    Remove from scene

--- WarFly/Cloud.swift
+++ WarFly/Cloud.swift
@@ -9,12 +9,14 @@ import GameplayKit
 
 
 final class Cloud: SKSpriteNode, GameBackGroundSpriteable {
-    static func populate() -> Cloud {
+    static func populate(at point: CGPoint?) -> Cloud {
         let cloudImageName = configureName()
         let cloud = Cloud(imageNamed: cloudImageName)
         cloud.setScale(randomScaleFactor)
-        cloud.position = randomPoint()
+        cloud.position = point ?? randomPoint()
         cloud.zPosition = CGFloat(Int.random(in: 19...21))
+        cloud.name = "backgroundSprite"
+        cloud.anchorPoint = CGPoint(x: 0.5, y: 1.0)
         cloud.run(move(from: cloud.position))
         
         return cloud
--- WarFly/GameScene.swift
+++ WarFly/GameScene.swift
@@ -25,7 +25,7 @@ class GameScene: SKScene {
     fileprivate func spawnClouds() {
         let spawnCloudWait = SKAction.wait(forDuration: 1)
         let spawnClowdAction = SKAction.run {
-            let cloud = Cloud.populate()
+            let cloud = Cloud.populate(at: nil)
             self.addChild(cloud)
         }
         let spawnCloudSequence = SKAction.sequence([spawnCloudWait, spawnClowdAction])
@@ -36,7 +36,7 @@ class GameScene: SKScene {
     fileprivate func spawnIslands() {
         let spawnIslandWait = SKAction.wait(forDuration: 1.5)
         let spawnIslandAction = SKAction.run {
-            let island = Island.populate()
+            let island = Island.populate(at: nil)
             self.addChild(island)
         }
         let spawnIslandSequence = SKAction.sequence([spawnIslandWait, spawnIslandAction])
@@ -79,6 +79,11 @@ class GameScene: SKScene {
             player.position.x = self.size.width + 70
         } else if player.position.x > self.size.width + 70 {
             player.position.x = -70
+        }
+        enumerateChildNodes(withName: "backgroundSprite") { (node, stop) in
+            if node.position.y < -199 {
+                node.removeFromParent()
+            }
         }
     }
 }
