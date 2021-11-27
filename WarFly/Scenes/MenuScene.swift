5403831c39b390e2a5fad96aaeebf286642e826d
Areg Vardanian <aregv@bk.ru>
2021-11-27 14:31:35 +0300

    Collision explosion

--- WarFly/Scenes/MenuScene.swift
+++ WarFly/Scenes/MenuScene.swift
@@ -16,8 +16,6 @@ class MenuScene: ParentScene {
             Assets.shared.isLoaded = true
         }
         
-        self.backgroundColor = SKColor(red: 0.15, green: 0.15, blue: 0.3, alpha: 1.0)
-        
         setHeader(withName: nil, andBackground: "header1")
         
         let titles = ["play", "options", "best"]
@@ -51,12 +49,11 @@ class MenuScene: ParentScene {
             
         }  else if node.name == "best" {
             
-//            let transition = SKTransition.crossFade(withDuration: 1.0)
-//            let bestScene = OptionsScene(size: self.size)
-//            optionsScene.backScene = self
-//            optionsScene.scaleMode = .aspectFill
-//            self.scene!.view?.presentScene(optionsScene, transition: transition)
-//
+            let transition = SKTransition.crossFade(withDuration: 0.3)
+            let bestScene = BestScene(size: self.size)
+            bestScene.backScene = self
+            bestScene.scaleMode = .aspectFill
+            self.scene!.view?.presentScene(bestScene, transition: transition)
         }
     }
 }
